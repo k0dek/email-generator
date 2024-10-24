00779506385304ad7e3b4e8a0141e5cedd9c2c06
Serghei Mamulat <s.mamulat@brainoza.com>
2024-10-24 09:02:15 +0300

    A

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export function App() {
     <>
     <div className="min-h-screen pt-[30px] bg-gradient-to-br from-indigo-50 via-white to-purple-50">
       <Navbar />
-
+{/* aksldjakl */}
       <main>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-[130px] lg:pb-[170px] sm:pb-[90px]">
           <div className="text-center mb-12">
